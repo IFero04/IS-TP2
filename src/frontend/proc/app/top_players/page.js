708a5e82fe543a8d4712bcbf55010e0cba384069
Afonso Faria <afonsofariatech@gmail.com>
2024-01-18 20:54:38 +0000

    Top Player HAndle Graphql

--- src/frontend/proc/app/top_players/page.js
+++ src/frontend/proc/app/top_players/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import {
+  Grid,
   Container,
   FormControl,
   InputLabel,
@@ -18,24 +19,25 @@ import {
 } from "@mui/material";
 
 function TopPlayers() {
-  const [selectedYearRange, setSelectedYearRange] = useState("");
   const [procData, setProcData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [graphqlData, setGraphqlData] = useState(null);
   const [orderBy, setOrderBy] = useState("");
   const [order, setOrder] = useState("asc");
   const [availableYears, setAvailableYears] = useState([]);
+  const [selectedYearRange, setSelectedYearRange] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setProcData(null);
-
-    const apiUrl = `http://${process.env.NEXT_PUBLIC_API_PROC_URL}/api/topPlayers`;
-
     setLoading(true);
+    setProcData(null);
+    setGraphqlData(null);
 
-    fetch(apiUrl)
+    const apiUrlProc = `http://${process.env.NEXT_PUBLIC_API_PROC_URL}/api/topPlayers`;
+
+    fetch(apiUrlProc)
       .then((res) => res.json())
       .then((data) => {
-        console.log(`Fetched data from ${apiUrl}`);
+        console.log(`Fetched data from ${apiUrlProc}`);
         if (data.result && data.result.length > 0) {
           const years = data.result.map((player) => parseInt(player.draft_year, 10));
           const startYear = Math.min(...years);
@@ -51,12 +53,13 @@ function TopPlayers() {
         }
       })
       .catch((error) => {
-        console.error(`Error fetching data from ${apiUrl}:`, error);
+        console.error(`Error fetching data from ${apiUrlProc}:`, error);
       })
       .finally(() => {
         setLoading(false);
       });
   }, [selectedYearRange]);
+
 
   const generateYearList = (startYear, endYear) => {
     const yearList = [];
@@ -72,7 +75,7 @@ function TopPlayers() {
     setOrderBy(column);
   };
 
-  const sortedData = procData
+  const sortedProcData = procData
     ? [...procData].sort((a, b) => {
         if (order === "asc") {
           return a[orderBy] > b[orderBy] ? 1 : -1;
@@ -82,9 +85,19 @@ function TopPlayers() {
       })
     : null;
 
+  const sortedGraphqlData = graphqlData
+    ? [...graphqlData].sort((a, b) => {
+        if (order === "asc") {
+          return a[orderBy] > b[orderBy] ? 1 : -1;
+        } else {
+          return a[orderBy] < b[orderBy] ? 1 : -1;
+        }
+      })
+    : null;
+
   return (
-    <Container maxWidth="md" style={{ marginTop: "2rem" }}>
-      <Typography variant="h4" gutterBottom>
+    <Container maxWidth="xl">
+      <Typography variant="h3" align="center" gutterBottom >
         Top Players
       </Typography>
 
@@ -112,105 +125,225 @@ function TopPlayers() {
       {loading ? (
         <CircularProgress style={{ marginTop: "1rem" }} />
       ) : (
-        <TableContainer style={{ marginTop: "1rem" }}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <TableSortLabel
-                    active={orderBy === "name"}
-                    direction={orderBy === "name" ? order : "asc"}
-                    onClick={() => handleSort("name")}
-                  >
-                    Name
-                  </TableSortLabel>
-                </TableCell>
-                <TableCell>
-                  <TableSortLabel
-                    active={orderBy === "age"}
-                    direction={orderBy === "age" ? order : "asc"}
-                    onClick={() => handleSort("age")}
-                  >
-                    Age
-                  </TableSortLabel>
-                </TableCell>
-                <TableCell>
-                  <TableSortLabel
-                    active={orderBy === "country"}
-                    direction={orderBy === "country" ? order : "asc"}
-                    onClick={() => handleSort("country")}
-                  >
-                    Country
-                  </TableSortLabel>
-                </TableCell>
-                <TableCell>
-                  <TableSortLabel
-                    active={orderBy === "draft_number"}
-                    direction={orderBy === "draft_number" ? order : "asc"}
-                    onClick={() => handleSort("draft_number")}
-                  >
-                    Draft Number
-                  </TableSortLabel>
-                </TableCell>
-                <TableCell>
-                  <TableSortLabel
-                    active={orderBy === "draft_round"}
-                    direction={orderBy === "draft_round" ? order : "asc"}
-                    onClick={() => handleSort("draft_round")}
-                  >
-                    Draft Round
-                  </TableSortLabel>
-                </TableCell>
-                <TableCell>
-                  <TableSortLabel
-                    active={orderBy === "height"}
-                    direction={orderBy === "height" ? order : "asc"}
-                    onClick={() => handleSort("height")}
-                  >
-                    Height
-                  </TableSortLabel>
-                </TableCell>
-                <TableCell>
-                  <TableSortLabel
-                    active={orderBy === "weight"}
-                    direction={orderBy === "weight" ? order : "asc"}
-                    onClick={() => handleSort("weight")}
-                  >
-                    Weight
-                  </TableSortLabel>
-                </TableCell>
-                <TableCell>
-                  <TableSortLabel
-                    active={orderBy === "draft_year"}
-                    direction={orderBy === "draft_year" ? order : "asc"}
-                    onClick={() => handleSort("draft_year")}
-                  >
-                    Year
-                  </TableSortLabel>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {sortedData ? (
-                sortedData.map((data) => (
-                  <TableRow key={data.name}>
-                    <TableCell>{data.name}</TableCell>
-                    <TableCell>{data.age}</TableCell>
-                    <TableCell>{data.country}</TableCell>
-                    <TableCell>{data.draft_number}</TableCell>
-                    <TableCell>{data.draft_round}</TableCell>
-                    <TableCell>{data.height}</TableCell>
-                    <TableCell>{data.weight}</TableCell>
-                    <TableCell>{data.draft_year}</TableCell>
+        <Grid container spacing={3} style={{ marginTop: "1rem" }}>
+          <Grid item xs={12} sm={6} style={{ textAlign: "left" }}>
+            <Typography variant="h5" align="center" gutterBottom>
+              Data from Proc API
+            </Typography>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "name"}
+                        direction={orderBy === "name" ? order : "asc"}
+                        onClick={() => handleSort("name")}
+                      >
+                        Name
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "age"}
+                        direction={orderBy === "age" ? order : "asc"}
+                        onClick={() => handleSort("age")}
+                      >
+                        Age
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "country"}
+                        direction={orderBy === "country" ? order : "asc"}
+                        onClick={() => handleSort("country")}
+                      >
+                        Country
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "draft_number"}
+                        direction={orderBy === "draft_number" ? order : "asc"}
+                        onClick={() => handleSort("draft_number")}
+                      >
+                        Draft Number
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "draft_round"}
+                        direction={orderBy === "draft_round" ? order : "asc"}
+                        onClick={() => handleSort("draft_round")}
+                      >
+                        Draft Round
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "height"}
+                        direction={orderBy === "height" ? order : "asc"}
+                        onClick={() => handleSort("height")}
+                      >
+                        Height
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "weight"}
+                        direction={orderBy === "weight" ? order : "asc"}
+                        onClick={() => handleSort("weight")}
+                      >
+                        Weight
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "draft_year"}
+                        direction={orderBy === "draft_year" ? order : "asc"}
+                        onClick={() => handleSort("draft_year")}
+                      >
+                        Year
+                      </TableSortLabel>
+                    </TableCell>
                   </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={8}>No players data available</TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
+                </TableHead>
+                <TableBody>
+                  {sortedProcData ? (
+                    sortedProcData.map((data) => (
+                      <TableRow key={data.name}>
+                        <TableCell>{data.name}</TableCell>
+                        <TableCell>{data.age}</TableCell>
+                        <TableCell>{data.country}</TableCell>
+                        <TableCell>{data.draft_number}</TableCell>
+                        <TableCell>{data.draft_round}</TableCell>
+                        <TableCell>{data.height}</TableCell>
+                        <TableCell>{data.weight}</TableCell>
+                        <TableCell>{data.draft_year}</TableCell>
+                      </TableRow>
+                    ))
+                  ) : (
+                    <TableRow>
+                      <TableCell colSpan={8}>No players data available</TableCell>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item xs={12} sm={6} style={{ textAlign: "center" }}>
+            <Typography variant="h5" align="center" gutterBottom>
+              Data from GraphQL API
+            </Typography>
+          <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "name"}
+                        direction={orderBy === "name" ? order : "asc"}
+                        onClick={() => handleSort("name")}
+                      >
+                        Name
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "age"}
+                        direction={orderBy === "age" ? order : "asc"}
+                        onClick={() => handleSort("age")}
+                      >
+                        Age
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "country"}
+                        direction={orderBy === "country" ? order : "asc"}
+                        onClick={() => handleSort("country")}
+                      >
+                        Country
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "draft_number"}
+                        direction={orderBy === "draft_number" ? order : "asc"}
+                        onClick={() => handleSort("draft_number")}
+                      >
+                        Draft Number
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "draft_round"}
+                        direction={orderBy === "draft_round" ? order : "asc"}
+                        onClick={() => handleSort("draft_round")}
+                      >
+                        Draft Round
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "height"}
+                        direction={orderBy === "height" ? order : "asc"}
+                        onClick={() => handleSort("height")}
+                      >
+                        Height
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "weight"}
+                        direction={orderBy === "weight" ? order : "asc"}
+                        onClick={() => handleSort("weight")}
+                      >
+                        Weight
+                      </TableSortLabel>
+                    </TableCell>
+                    <TableCell>
+                      <TableSortLabel
+                        active={orderBy === "draft_year"}
+                        direction={orderBy === "draft_year" ? order : "asc"}
+                        onClick={() => handleSort("draft_year")}
+                      >
+                        Year
+                      </TableSortLabel>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {sortedGraphqlData ? (
+                    sortedGraphqlData.map((data) => (
+                      <TableRow key={data.name}>
+                        <TableCell>{data.name}</TableCell>
+                        <TableCell>{data.age}</TableCell>
+                        <TableCell>{data.country}</TableCell>
+                        <TableCell>{data.draft_number}</TableCell>
+                        <TableCell>{data.draft_round}</TableCell>
+                        <TableCell>{data.height}</TableCell>
+                        <TableCell>{data.weight}</TableCell>
+                        <TableCell>{data.draft_year}</TableCell>
+                      </TableRow>
+                    ))
+                  ) : (
+                    <TableRow>
+                      <TableCell colSpan={8}>No players data available</TableCell>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Grid>
+      )}
+
+      {loading ? (
+        <CircularProgress style={{ marginTop: "1rem" }} />
+      ) : (
+        <TableContainer style={{ marginTop: "1rem" }}>
+          
         </TableContainer>
       )}
     </Container>
